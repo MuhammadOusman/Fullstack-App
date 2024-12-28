3880b5e9aabf87ec39af10c7dfa7f49f4268e569
sleepless-404 <ousmansohail786@gmail.com>
2024-12-28 17:13:40 +0500

    json parsing middlware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
+import { json, urlencoded } from 'express';
 import express  from 'express';
 import productRouter from './routes/products/index';
 const port=3000;
 
 const app=express();
+
+app.use(json());
+app.use(urlencoded({extended:false}));
+
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
 app.use('/products',productRouter);
-
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
